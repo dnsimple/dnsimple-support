4a7f6de3c32a88e641d787e422119477b063279d
Simone Carletti <weppos@weppos.net>
2014-02-20 13:07:48 +0100

    Add a super simple search input that forwards to Google
    Temporary workaround for GH-1

---
+++ content/assets/js/script.js
@@ -0,0 +1,13 @@
+(function($){
+
+  $(function() {
+
+    // Temporary hack the search for to narrow the search to the current site.
+    $("#search-form").submit(function(event) {
+      var $searchInput = $(this).find("input[type=search]");
+      $searchInput.val("site:support.dnsimple.com " + $searchInput.val());
+    });
+
+})
+
+})(jQuery);
