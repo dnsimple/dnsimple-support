f294dae3d2cf9d65fcfa948f7ed518230193803b
Sebastian Hermida <sebas00@gmail.com>
2019-06-25 12:42:09 +0200

    Manage FontAwesome through Yarn

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
-
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'content/dist')
-  }
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'node_modules/@fortawesome/fontawesome-free', to: 'fontawesome' },
+    ]),
+  ],
 };
