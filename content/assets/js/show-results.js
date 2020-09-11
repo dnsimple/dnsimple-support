4a5514ff7e8d7b9c57d46a85cb49a256725f616e
Dallas Read <dallas@excitecreative.ca>
2020-09-11 20:59:14 -0300

    es2015

--- content/assets/js/show-results.js
+++ content/assets/js/show-results.js
@@ -1,55 +1,50 @@
 (function () {
-  const $main = document.getElementById('main');
-  const $input = document.getElementById('input-search');
+  var $main = document.getElementById("main");
+  var $input = document.getElementById("input-search"); // Decodes query parameter for search
 
-  // Decodes query parameter for search
-  const parseQueryParams = function () {
-    return decodeURIComponent(window.location.search.substring(3).replace(/\+/g, ' '));
-  };
+  var parseQueryParams = function parseQueryParams() {
+    return decodeURIComponent(
+      window.location.search.substring(3).replace(/\+/g, " ")
+    );
+  }; // Search based on a query parameter
 
-  // Search based on a query parameter
-  const search = function (q) {
+  var search = function search(q) {
     $input.value = q;
     return window.DNSimpleSupport.search(q);
   };
 
-  const showResults = (q) => {
-    const results = search(q);
-    const $results = document.createElement('div');
-    const $h1 = document.createElement('h1');
-    const $ul = document.createElement('ul');
-
-    $h1.innerText = 'Search results';
-
-    if (q.length)
-      $h1.innerText += ` for "${q}"`;
+  var showResults = function showResults(q) {
+    var results = search(q);
+    var $results = document.createElement("div");
+    var $h1 = document.createElement("h1");
+    var $ul = document.createElement("ul");
+    $h1.innerText = "Search results";
+    if (q.length) $h1.innerText += ' for "' + q + '"';
 
     if (results.length === 0) {
-      const $li = document.createElement('li');
-      $li.innerText = 'No results found';
+      var $li = document.createElement("li");
+      $li.innerText = "No results found";
       $ul.appendChild($li);
     }
 
     results.forEach(function (result) {
-      const $li = document.createElement('li');
-      const $a = document.createElement('a');
+      var $li = document.createElement("li");
+      var $a = document.createElement("a");
       $a.href = result.id;
       $a.innerHTML = result.title;
       $li.appendChild($a);
       $ul.append($li);
     });
-
     $results.appendChild($h1);
     $results.appendChild($ul);
-
-    $main.innerHTML = '';
+    $main.innerHTML = "";
     $main.appendChild($results);
   };
 
-  const q = parseQueryParams();
+  var q = parseQueryParams();
+  if (q.length) showResults(q);
 
-  if (q.length)
-    showResults(q);
-
-  $input.oninput = (e) => showResults($input.value);
+  $input.oninput = function (e) {
+    return showResults($input.value);
+  };
 })();
