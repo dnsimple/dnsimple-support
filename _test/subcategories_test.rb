6d2cbeff137f4e27860a31f95cacae22cc78146c
Sebastian Hermida <sebas00@gmail.com>
2023-04-14 11:18:23 +0200

    Rubocop cleanup

--- _test/subcategories_test.rb
+++ _test/subcategories_test.rb
@@ -1,24 +1,25 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 require_relative '../lib/sub_categories'
 
-describe "Sub categories" do
+FakeArticle = Struct.new(:attributes)
 
-  FakeArticle = Struct.new(:attributes)
+describe 'Sub categories' do
+  it 'replace the title for the nanoc article' do
+    fake_article = FakeArticle.new({ title: 'What is a TLD?' })
 
-  it "replace the title for the nanoc article" do
-    fake_article = FakeArticle.new({ title: "What is a TLD?" })
+    result = SubCategories.new.show('domains', [fake_article]) # Loads the domains.yaml file
 
-    result = SubCategories.new.show("domains", [fake_article]) # Loads the domains.yaml file
-
-    assert_equal result["Top-level domains"][0], fake_article
+    assert_equal result['Top-level domains'][0], fake_article
   end
 
   it "the article is not categorized, it goes into the 'other' section" do
-    fake_article = FakeArticle.new({ title: "Dummy Article" })
+    fake_article = FakeArticle.new({ title: 'Dummy Article' })
 
-    result = SubCategories.new.show("domains", [fake_article]) # loads the domains.yaml file
+    result = SubCategories.new.show('domains', [fake_article]) # loads the domains.yaml file
 
-    assert_equal result["Other"][0], fake_article
+    assert_equal result['Other'][0], fake_article
   end
 
 end
