80afef0c56031addeed5aa1fb82cc80fa3efd2b8
Simone Carletti <weppos@weppos.net>
2024-02-20 14:45:22 +0100

    Switch mardown extension from .markdown to .md (#1132)
    We had a mix of both. Tests were expecting (and checking) only `.markdown`.
    
    I have updated the extension, updated the tests, and added a test to ensure we won't create more `.markdown` files.

---
+++ _test/posts_test.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+describe "Content" do
+  it "checks for banned characters in markdown files" do
+    banned_characters = [
+      "\u201C", # U+201C "
+      "\u201D", # U+201D "
+      "\u201E", # U+201E „
+      "\u2018", # U+2018 ‘
+      "\u2019", # U+2019 ’
+      "\u201A", # U+201A ‚
+      "\u201B", # U+201B ‛
+      "\u2026" # U+2020 …
+    ]
+
+    regexp   = /[#{banned_characters.join}]/
+    affected = []
+
+    Dir.glob("content/**/*.md").each do |path|
+      File.readlines(path).each do |line|
+        affected << [path, line] if regexp.match(line)
+      end
+    end
+
+    message = "#{affected.size} lines contain banned characters:\n"
+    affected.each_with_index do |(path, line), index|
+      message << "#{index + 1}. #{path} -> #{line}"
+    end
+
+    assert_equal 0, affected.size, message
+  end
+end
+
+describe "Links" do
+  it "ensures that links are valid" do
+    regexp = /(?<!\!)\[.*\]\((?!(http|\/articles|\/categories|\/files|#|mailto))[^\)]*\)/
+
+    affected = []
+
+    Dir.glob("content/**/*.md").each do |path|
+      next unless File.file?(path)
+
+      File.readlines(path).each do |line|
+        affected << [path, line] if regexp.match(line)
+      end
+    end
+
+    message = "#{affected.size} lines contain invalid links:\n"
+    affected.each_with_index do |(path, line), index|
+      message << "#{index + 1}. #{path} -> #{line}"
+    end
+
+    assert_equal 0, affected.size, message
+  end
+end
+
+describe "Files" do
+  it "checks that markdown file names are set to lowercase" do
+    Dir.glob("content/**/*.md").each do |path|
+      assert_equal path, path.downcase, "Markdown file name must be lowercase or it creates 3xx redirects in our sitemap. This is bad for SEO. Thank you!"
+    end
+  end
+
+  it "checks there are no files with .markdown extension" do
+    files_count = Dir.glob("content/**/*.markdown").count
+    assert_equal 0, files_count, "Markdown file extension must be .md"
+  end
+end
--- _test/subcategories_test.rb
+++ _test/subcategories_test.rb
@@ -1,25 +1,24 @@
 # frozen_string_literal: true
 
-require 'test_helper'
-require_relative '../lib/sub_categories'
+require "test_helper"
+require_relative "../lib/sub_categories"
 
 FakeArticle = Struct.new(:attributes)
 
-describe 'Sub categories' do
-  it 'replace the title for the nanoc article' do
-    fake_article = FakeArticle.new({ title: 'What is a TLD?' })
+describe "Sub categories" do
+  it "replace the title for the nanoc article" do
+    fake_article = FakeArticle.new({ title: "What is a TLD?" })
 
-    result = SubCategories.new.show('domains', [fake_article]) # Loads the domains.yaml file
+    result = SubCategories.new.show("domains", [fake_article]) # Loads the domains.yaml file
 
-    assert_equal result['Top-level domains'][0], fake_article
+    assert_equal result["Top-level domains"][0], fake_article
   end
 
   it "the article is not categorized, it goes into the 'other' section" do
-    fake_article = FakeArticle.new({ title: 'Dummy Article' })
+    fake_article = FakeArticle.new({ title: "Dummy Article" })
 
-    result = SubCategories.new.show('domains', [fake_article]) # loads the domains.yaml file
+    result = SubCategories.new.show("domains", [fake_article]) # loads the domains.yaml file
 
-    assert_equal result['Other'][0], fake_article
+    assert_equal result["Other"][0], fake_article
   end
-
 end
