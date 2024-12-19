a3d198358c13fb4a25a790ed7c7acb4b12bad1e8
Dallas Read <dallas.read@dnsimple.com>
2024-12-19 16:29:59 -0400

    Open with an opener

--- _widget/src/main.js
+++ _widget/src/main.js
@@ -4,9 +4,7 @@ import App from './components/app/component.vue';
 const $openers = [...document.querySelectorAll('[data-open-support-widget]')];
 const app = createApp(App, {
   showPrompt: $openers.length === 0
-});
-
-app.mount('#support-widget');
+}).mount('#support-widget');
 
 $openers.forEach(($el) => {
   $el.addEventListener('click', () => {
