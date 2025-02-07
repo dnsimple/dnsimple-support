ce6b85cb33e9c4bccafaeccd28dd42f1f34762c2
Dallas Read <dallas.read@dnsimple.com>
2025-02-07 12:03:25 -0400

    Remove widget title

--- _widget/src/components/header/component.vue
+++ _widget/src/components/header/component.vue
@@ -3,7 +3,6 @@
         <a href="javascript:;" @click="app.close" class="minimize">
             <div v-html="minimizeIcon"></div>
         </a>
-        <h1>DNSimple Support</h1>
         <input
                 type="text"
                 ref="input"
