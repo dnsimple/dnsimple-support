674fb7cf4bf5c96dfec1559bd1d5be6f7437873b
Dallas Read <dallas.read@dnsimple.com>
2025-02-07 13:58:31 -0400

    Remove the DOM content loaded func

--- _widget/src/main.js
+++ _widget/src/main.js
@@ -1,27 +1,25 @@
 import { createApp } from 'vue';
 import App from './components/app/component.vue';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const elementId = 'dnsimple-support-widget';
-  const $openers = [...document.querySelectorAll('[data-dnsimple-open-support-widget]')];
+const elementId = 'dnsimple-support-widget';
+const $openers = [...document.querySelectorAll('[data-dnsimple-open-support-widget]')];
 
-  let $target = document.querySelector(`#${elementId}`);
+let $target = document.querySelector(`#${elementId}`);
 
-  if (!$target) {
-    $target = document.createElement('div');
-    $target.id = elementId;
-    document.body.insertBefore($target, document.body.firstChild);
-  }
+if (!$target) {
+  $target = document.createElement('div');
+  $target.id = elementId;
+  document.body.insertBefore($target, document.body.firstChild);
+}
 
-  const app = createApp(App, {
-    showPrompt: $openers.length === 0,
-    currentSiteUrl: document.querySelector('[data-dnsimple-current-site-url]')?.getAttribute('data-dnsimple-current-site-url') || ''
-  }).mount($target);
+const app = createApp(App, {
+  showPrompt: $openers.length === 0,
+  currentSiteUrl: document.querySelector('[data-dnsimple-current-site-url]')?.getAttribute('data-dnsimple-current-site-url') || ''
+}).mount($target);
 
-  $openers.forEach(($el) => {
-    $el.addEventListener('click', () => {
-      app.open();
-      app.focus();
-    });
+$openers.forEach(($el) => {
+  $el.addEventListener('click', () => {
+    app.open();
+    app.focus();
   });
 });
