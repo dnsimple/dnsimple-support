99d96b716ca2e5c963af6a930b2d174869460e97
Dallas Read <dallas.read@dnsimple.com>
2025-02-07 12:40:33 -0400

    Cleanup

--- _widget/src/main.js
+++ _widget/src/main.js
@@ -3,14 +3,14 @@ import App from './components/app/component.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   const elementId = 'dnsimple-support-widget';
-  const $openers = [...document.querySelectorAll('[data-open-support-widget]')];
+  const $openers = [...document.querySelectorAll('[data-dnsimple-open-support-widget]')];
 
   let $target = document.querySelector(`#${elementId}`);
 
   if (!$target) {
     $target = document.createElement('div');
     $target.id = elementId;
-    document.body.appendChild($target);
+    document.body.insertBefore($target, document.body.firstChild);
   }
 
   const app = createApp(App, {
