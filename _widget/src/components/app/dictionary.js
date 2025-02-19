9e323afe584aba55ce5f60bae809ee5a6beec994
Dallas Read <dallas.read@dnsimple.com>
2025-02-19 14:18:07 -0400

    Add comment to the dictionry

--- _widget/src/components/app/dictionary.js
+++ _widget/src/components/app/dictionary.js
@@ -22,5 +22,7 @@ export default {
   "stats": "query volume analytics",
   "observability": "query volume analytics",
   "queries": "query volume analytics",
-  "analytics": "query volume analytics"
+  "query": "query volume analytics",
+  "analytics": "query volume analytics",
+  "comment": "note"
 };
