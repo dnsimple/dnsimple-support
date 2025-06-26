fe58faef62e3796226e39821234fc545d9068665
Steph <121963952+stephanoodle@users.noreply.github.com>
2025-06-26 11:21:00 -0700

    Create disabling-dnssec.md (#1514)
    * Create disabling-dnssec.md
    
    Adds Disabling DNSSEC Support Article
    
    * Update disabling-dnssec.md
    
    * updates H2
    
    * Update disabling-dnssec.md
    
    * fixing tests for DNSSec
    
    * fixes formatting error
    
    ---------
    
    Co-authored-by: Alyse <42185139+itsalyse@users.noreply.github.com>
    Co-authored-by: David Morrow <dperrymorrow@gmail.com>

--- spec/_widget/components/search.spec.js
+++ spec/_widget/components/search.spec.js
@@ -87,10 +87,11 @@ describe('Search', () => {
         'Account Activation': 5
       },
       'enable dnssec': {
-        'DNSSEC': 2,
-        'DNSSEC Glossary': 3,
-        'Using Cloudflare DNSSEC with DNSimple': 4,
-        'Why DNSSEC and Secondary DNS may not work together': 5,
+        'Disabling DNSSEC': 2,
+        'DNSSEC': 3,
+        'DNSSEC Glossary': 4,
+        'Using Cloudflare DNSSEC with DNSimple': 5,
+        'Why DNSSEC and Secondary DNS may not work together': 6,
       },
       'creating alias record': {
         'What\'s an ALIAS record?': 1,
