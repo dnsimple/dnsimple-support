3bf36f354037762f91392f90b9b897c1c930be70
Grant Butler <gronk.droid@gmail.com>
2025-11-19 00:54:53 -0500

    Fix test catching

--- spec/_widget/components/search.spec.js
+++ spec/_widget/components/search.spec.js
@@ -151,7 +151,6 @@ describe("Search", () => {
         "guide-getting-started-with-your-team",
         "multi-factor-authentication",
       ],
-      "domain parking": ["dns-hosting"],
       "dns propagation": ["domain-resolution-issues", "record-resolution-issues"],
       "dnsimple pricing": ["dnsimple-plans", "domain-transfer-pricing"],
       "change nameservers": ["setting-name-servers", "what-is-a-nameserver"],
