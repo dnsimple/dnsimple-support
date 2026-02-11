6c213daf6945838eb76bf8665f10fad555848242
Steph <stephanoodle@users.noreply.github.com>
2026-02-11 13:14:38 -0800

    Remove TLD/Explanation section from Domains and Transfers (now in TLDs category)

--- _test/subcategories_test.rb
+++ _test/subcategories_test.rb
@@ -7,11 +7,11 @@ FakeArticle = Struct.new(:attributes)
 
 describe "Sub categories" do
   it "replace the title for the nanoc article" do
-    fake_article = FakeArticle.new({ title: "What is a TLD?" })
+    fake_article = FakeArticle.new({ title: "Registering a Domain" })
 
     result = SubCategories.new.show("Domains and Transfers", [fake_article]) # Loads the domains_and_transfers.yaml file
 
-    assert_equal result["Explanation"]["TLD Basics"][0], fake_article
+    assert_equal result["How-to"]["Domain Registration & Setup"][1], fake_article
   end
 
   it "the article is not categorized, it goes into the 'other' section" do
