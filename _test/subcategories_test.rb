666805d3c283d3b7e1664ff919dffad1bf724671
Steph <stephanoodle@users.noreply.github.com>
2026-02-11 14:01:02 -0800

    Fix subcategories test for Domains and Transfers category

--- _test/subcategories_test.rb
+++ _test/subcategories_test.rb
@@ -7,17 +7,17 @@ FakeArticle = Struct.new(:attributes)
 
 describe "Sub categories" do
   it "replace the title for the nanoc article" do
-    fake_article = FakeArticle.new({ title: "What is a TLD?" })
+    fake_article = FakeArticle.new({ title: "What Happens When a Domain Expires?" })
 
-    result = SubCategories.new.show("domains", [fake_article]) # Loads the domains.yaml file
+    result = SubCategories.new.show("Domains and Transfers", [fake_article])
 
-    assert_equal result["Top-level domains"][0], fake_article
+    assert_equal result["Explanation"]["Domain Lifecycle"][0], fake_article
   end
 
   it "the article is not categorized, it goes into the 'other' section" do
     fake_article = FakeArticle.new({ title: "Dummy Article" })
 
-    result = SubCategories.new.show("domains", [fake_article]) # loads the domains.yaml file
+    result = SubCategories.new.show("Domains and Transfers", [fake_article])
 
     assert_equal result["Other"][0], fake_article
   end
